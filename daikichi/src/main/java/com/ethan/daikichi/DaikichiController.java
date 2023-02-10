5cca750a1296244875b1f8591a2d6c33652b2cf2
Ethan Smith <EthanKSmith@gmail.com>
2023-02-10 10:54:43 -0700

    Improved Request Mapping

--- daikichi/src/main/java/com/ethan/daikichi/DaikichiController.java
+++ daikichi/src/main/java/com/ethan/daikichi/DaikichiController.java
@@ -4,19 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikichi")
 public class DaikichiController {
 
-  @RequestMapping("/daikichi")
+  @RequestMapping("")
   public String index(){
     return "Hello JOAN";
   }
 
-  @RequestMapping("/daikichi/today")
+  @RequestMapping("/today")
   public String today(){
     return "Today you will find luck in all areas.";
   }
 
-  @RequestMapping("/daikichi/tomorrow")
+  @RequestMapping("/tomorrow")
   public String tomorrow(){
     return "Tomorrow, an opportunity will arise!";
   }
